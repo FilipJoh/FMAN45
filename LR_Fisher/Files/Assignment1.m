7ea33433923bcb06e38c4c616b32453e57c45483
FilipJoh <filip.johannesson@gmail.com>
2016-11-21 11:43:15 +0100

    Last assignment started

--- LR_Fisher/Files/Assignment1.m
+++ LR_Fisher/Files/Assignment1.m
@@ -1,3 +1,52 @@
 % load data
 load data.mat
 
+
+w1L=logisticreg(train1.X,train1.y);
+w2L=logisticreg(train2.X,train2.y);
+w3L=logisticreg(train3.X,train3.y);
+
+w1F=fisherdiscriminant(train1.X, train1.y);
+w2F=fisherdiscriminant(train2.X, train2.y);
+w3F=fisherdiscriminant(train3.X, train3.y);
+
+
+weights=[w1L w1F w2L w2F w3L w3F];
+weightsn={'w1L' 'w1F' 'w2L' 'w2F' 'w3L' 'w3F'};
+
+for i=1:6
+   if(i<3)
+      errorez(i)= errorrate(weights(:,i),test1);
+      disp(['Errors for: ' weightsn{i} ' ' num2str( errorez(i))])
+   elseif(i<5)
+      errorez(i)= errorrate(weights(:,i),test2);
+      disp(['Errors for: ' weightsn{i} ' ' num2str( errorez(i))])
+   else
+      errorez(i)= errorrate(weights(:,i),test3);
+      disp(['Errors for: ' weightsn{i} ' ' num2str( errorez(i))])
+   end
+end
+
+%Boundaries
+figure;
+boundary(weights(:,1:2),test1);
+legend('Points group1','Points group2','Logistic reg','Fisher');
+figure;
+boundary(weights(:,3:4),test2);
+legend('Points group1','Points group2','Logistic reg','Fisher');
+figure;
+boundary(weights(:,5:6),test3);
+legend('Points group1','Points group2','Logistic reg','Fisher');
+
+
+
+
+%4.
+wL4=logisticreg(train1_2.X,train1_2.y);
+wF4=fisherdiscriminant(train1_2.X, train1_2.y);
+errL=errorrate(wL4,test1)
+errF=errorrate(wF4,test1)
+figure;
+boundary([wL4 wF4],test1);
+legend('Points group1','Points group2','Logistic reg','Fisher');
+
